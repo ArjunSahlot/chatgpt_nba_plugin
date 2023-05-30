62e49cbf3905690a9f7ca641dfa01eac01837b73
ArjunSahlot <iarjun.sahlot@gmail.com>
2023-05-30 10:21:02 -0700

    base plugin

---
+++ legal.md
@@ -0,0 +1,25 @@
+# NBA Plugin Legal Information
+
+## Privacy Policy
+
+This plugin provides information about NBA players. It uses the `nba_api` to fetch the relevant data. This API, to the best of our knowledge, does not collect, store, or share any personal information of its own. It only provides information based on the player's name you input.
+
+The personal information that you provide (i.e., the player's name) is used solely for the purpose of fetching and displaying information about the requested player. We do not store, share, or use this information for any other purposes.
+
+## Terms of Use
+
+This plugin is intended for personal and non-commercial use. Please do not use it for any illegal activities, such as harassment, spamming, or any activity that violates the rights of others.
+
+## Data Accuracy
+
+The data provided by this plugin comes directly from the `nba_api` python package ([github](https://github.com/swar/nba_api)) and is, to the best of our knowledge, accurate at the time of retrieval. However, we cannot guarantee the accuracy or completeness of this data. Use it at your own risk and discretion.
+
+## Liability
+
+Under no circumstances will the creators of this plugin be held liable for any damages arising out of the use or inability to use this plugin. This includes, but is not limited to, damages for loss of profits, goodwill, use, data, or other intangible losses.
+
+## Contact Information
+
+If you have any questions or concerns about this plugin or these terms, please contact Arjun Sahlot at [iarjun.sahlot@gmail.com](mailto:iarjun.sahlot@gmail.com).
+
+By using this plugin, you agree to the above terms and conditions.
---
+++ main.py
@@ -0,0 +1,91 @@
+import json
+from nba_api.stats.static import players, teams
+from nba_api.stats.endpoints import commonplayerinfo, playercareerstats
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, HTMLResponse
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://chat.openai.com"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/")
+async def home():
+    html_content = """
+    """
+    return HTMLResponse(content=html_content)
+
+@app.get("/icon.jpg")
+async def get_icon():
+    return FileResponse("icon.jpg", media_type="image/jpeg")
+
+@app.get("/legal.md")
+async def get_legal():
+    return FileResponse("legal.md", media_type="text/markdown")
+
+@app.get("/.well-known/ai-plugin.json")
+async def plugin_manifest():
+    with open("ai-plugin.json") as f:
+        return json.load(f)
+
+
+@app.get("/player/{player_name}")
+async def player_info(player_name: str):
+    """
+    Given a player name, this will return some common info about the player.
+    nba_api.stats.endpoints.commonplayerinfo.CommonPlayerInfo is used to get the data.
+    """
+    player_id = players.find_players_by_full_name(player_name)[0]["id"]
+    common_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
+    data = {"info": {}}
+    for i in range(len(common_info["resultSets"][0]["headers"])):
+        data["info"][common_info["resultSets"][0]["headers"][i]] = common_info["resultSets"][0]["rowSet"][0][i]
+    
+    season_stats = common_info["resultSets"][1]["rowSet"][0][2]
+    data[f"stats_during_{season_stats}"] = {}
+    for i, category in enumerate(("PTS", "AST", "REB")):
+        data[f"stats_during_{season_stats}"][category] = common_info["resultSets"][1]["rowSet"][0][i+3]
+    
+    return data
+
+@app.get("/player/{player_name}/career")
+async def player_career(player_name: str):
+    player_id = players.find_players_by_full_name(player_name)[0]["id"]
+    career_df = playercareerstats.PlayerCareerStats(player_id=player_id).get_data_frames()[0]
+    career_dict = career_df.to_dict()
+    data = {"PLAYER_ID": player_id, "career": {}}
+    season_list = list(career_dict["SEASON_ID"].values())
+    for season in season_list:
+        data["career"][season] = {}
+
+    for key, stat in career_dict.items():
+        if key in ("PLAYER_ID", "SEASON_ID", "LEAGUE_ID", "TEAM_ID"):
+            continue
+        for ind, val in stat.items():
+            data["career"][season_list[ind]][key] = val
+    
+    return data
+
+
+@app.get("/player/{player_name}/{season}")
+async def player_specific(player_name: str, season: str):
+    """
+    Specific stats for a player in a specific season.
+    Season must be in the format: "YYYY-YY", ex: 2022-23
+    """
+    if len(season) == 9:
+        season = season[0:5] + season[7:9]
+    player_id = players.find_players_by_full_name(player_name)[0]["id"]
+    career_df = playercareerstats.PlayerCareerStats(player_id=player_id).get_data_frames()[0]
+    season_stats = career_df[career_df["SEASON_ID"] == season]
+    return {key: stat[list(stat.keys())[0]] for key, stat in season_stats.to_dict().items()}
+
+
+# @app.get("/team/{team_name}")
+# async def team_info(team_name: str):
+#     team = teams.find_teams_by_full_name(team_name)[0]
