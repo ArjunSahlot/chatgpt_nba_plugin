7662db72a1de26b2815a8fe88a1037c9ff0ff4a2
ArjunSahlot <iarjun.sahlot@gmail.com>
2024-01-06 21:33:03 -0800

    debug

--- main.py
+++ main.py
@@ -8,28 +8,46 @@ from fastapi.responses import FileResponse, HTMLResponse
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://chat.openai.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
+
 @app.get("/")
 async def home():
     html_content = """
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>NBA API ChatGPT Plugin</title>
+        </head>
+        <body>
+            <h1>NBA API ChatGPT Plugin</h1>
+            <p>Created by <a href="https://github.com/ArjunSahlot">Arjun Sahlot</a></p>
+            <p>Source code available on <a href="https://github.com/ArjunSahlot/chatgpt-nba-plugin">GitHub</a></p>
+        </body>
+    </html>
     """
     return HTMLResponse(content=html_content)
 
+
 @app.get("/icon.jpg")
 async def get_icon():
+    print("icon")
     return FileResponse("icon.jpg", media_type="image/jpeg")
+
 
 @app.get("/legal.md")
 async def get_legal():
+    print("legal")
     return FileResponse("legal.md", media_type="text/markdown")
+
 
 @app.get("/.well-known/ai-plugin.json")
 async def plugin_manifest():
+    print("manifest")
     with open("ai-plugin.json") as f:
         return json.load(f)
 
@@ -40,21 +58,24 @@ async def player_info(player_name: str):
     Given a player name, this will return some common info about the player.
     nba_api.stats.endpoints.commonplayerinfo.CommonPlayerInfo is used to get the data.
     """
+    print("player")
     player_id = players.find_players_by_full_name(player_name)[0]["id"]
     common_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_dict()
     data = {"info": {}}
     for i in range(len(common_info["resultSets"][0]["headers"])):
         data["info"][common_info["resultSets"][0]["headers"][i]] = common_info["resultSets"][0]["rowSet"][0][i]
-    
+
     season_stats = common_info["resultSets"][1]["rowSet"][0][2]
     data[f"stats_during_{season_stats}"] = {}
     for i, category in enumerate(("PTS", "AST", "REB")):
         data[f"stats_during_{season_stats}"][category] = common_info["resultSets"][1]["rowSet"][0][i+3]
-    
+
     return data
+
 
 @app.get("/player/{player_name}/career")
 async def player_career(player_name: str):
+    print("career")
     player_id = players.find_players_by_full_name(player_name)[0]["id"]
     career_df = playercareerstats.PlayerCareerStats(player_id=player_id).get_data_frames()[0]
     career_dict = career_df.to_dict()
@@ -68,7 +89,7 @@ async def player_career(player_name: str):
             continue
         for ind, val in stat.items():
             data["career"][season_list[ind]][key] = val
-    
+
     return data
 
 
@@ -78,6 +99,7 @@ async def player_specific(player_name: str, season: str):
     Specific stats for a player in a specific season.
     Season must be in the format: "YYYY-YY", ex: 2022-23
     """
+    print("specific")
     if len(season) == 9:
         season = season[0:5] + season[7:9]
     player_id = players.find_players_by_full_name(player_name)[0]["id"]
